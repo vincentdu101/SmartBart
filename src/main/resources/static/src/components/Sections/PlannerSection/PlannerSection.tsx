2930561ac5a90e71b3ebe14725db6adbc7922ac5
vincentdu101 <vincentdu101@gmail.com>
2018-09-03 22:30:59 -0700

    load stations info into planner section

--- src/main/resources/static/src/components/Sections/PlannerSection/PlannerSection.tsx
+++ src/main/resources/static/src/components/Sections/PlannerSection/PlannerSection.tsx
@@ -3,19 +3,31 @@ import "./PlannerSection.scss";
 import { IPlannerSectionState, IPlannerSectionProps } from "../../../types/PlannerTypes";
 import PlannerTable from "../../PlannerTable/PlannerTable";
 import DropdownInfo from "../../Dropdown/DropdownInfo";
+import * as ConfigService from "../../../services/Config/ConfigService";
 
 export default class PlannerSection extends React.Component<{}, IPlannerSectionState> {
 
     constructor(props: IPlannerSectionProps) {
         super(props);
 
+        this.loadStationsInfo.bind(this);
+
         this.state = {
             stations: []
         };
     }
 
-    public componentDidMount(): void {
+    private loadStationsInfo(): void {
+        this.setState({stations: []});
+        fetch(ConfigService.staticStationsInfo).then(results => {
+            return results.json();
+        }).then(data => {
+            this.setState({stations: data});
+        });
+    }
 
+    public componentDidMount(): void {
+        this.loadStationsInfo();
     }
 
     public render(): JSX.Element {
--- src/main/resources/static/src/services/Config/ConfigService.ts
+++ src/main/resources/static/src/services/Config/ConfigService.ts
@@ -1,1 +1,2 @@
 export const staticFilteredEstimates = "http://localhost:8080/planner/arrive/all";
+export const staticStationsInfo = "http://localhost:8080/stations/all";
