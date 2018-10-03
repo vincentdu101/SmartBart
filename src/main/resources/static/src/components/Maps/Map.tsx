786e35db46948fcc049ad455187554584e016a43
vincentdu <vdu@scilearn.com>
2018-10-03 08:45:43 -0700

    adding ticks and circles to the california map

--- src/main/resources/static/src/components/Maps/Map.tsx
+++ src/main/resources/static/src/components/Maps/Map.tsx
@@ -35,6 +35,48 @@ export class Map extends React.Component<IMapProps, IMapState> {
         this.setState({maps: nextProps.maps, stations: nextProps.stations});
     }
 
+    private generateCircles(): JSX.Element {
+        if (this.state.maps) {
+            const data = [[-122.490402, 37.786453], [-122.389809, 37.72728], [-78.917377, 39.757239], [-81.307761, 33.468848]];
+            // const data = topojson.feature(maps, maps.objects.counties).features;
+            const projection = d3.geoMercator()
+                                .scale(960)
+                                .center([-95.8, 37.9]);
+            return (
+                <TransitionGroup component={null}>
+                    {data.map((feature: [number, number], i) => {
+                        const fill = "steelblue";
+
+                        console.log(feature);
+                        return (
+                            <CSSTransition
+                                key={i}
+                                classNames={`state-transition-${i}`}
+                                appear={true}
+                                timeout={5000}>
+
+                                <g className="circle-container">
+                                    <circle
+                                        className={`states-circle raw state-transition-circle-${i}`}
+                                        r={5}
+                                        fill={fill}
+                                        stroke="#000000"
+                                        strokeWidth={0.5}
+                                        transform={'translate(' + projection(feature) + ')'}
+                                        opacity={0.75}
+                                    />
+                                </g>
+
+                            </CSSTransition>
+                        );
+                    })}
+                </TransitionGroup>
+            );
+        } else {
+            return (<div>test</div>);
+        }
+    }
+
     private generatePath(geoPath: any, data: any): JSX.Element | undefined {
         const mapType = "US";
 
@@ -94,6 +136,7 @@ export class Map extends React.Component<IMapProps, IMapState> {
                     viewBox="0 0 400 600"
                 >
                     {this.generateMap()}
+                    {this.generateCircles()}
                 </svg>
             </div>
         );
