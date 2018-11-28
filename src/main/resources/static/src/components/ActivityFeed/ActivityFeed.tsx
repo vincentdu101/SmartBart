72edb192e7072603758affab92a492270a0d31df
vincentdu <vdu@scilearn.com>
2018-11-27 17:47:14 -0800

    further refactor and implementation of activity feed

--- src/main/resources/static/src/components/ActivityFeed/ActivityFeed.tsx
+++ src/main/resources/static/src/components/ActivityFeed/ActivityFeed.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./ActivityFeed.css";
 import {IEtd, IEstimate} from "../../types/StationTypes";
 import {IActivityFeedProps, IActivityFeedState} from "../../types/ActivityTypes";
-import {Card, Collapse, ListGroup, ListGroupItem } from "reactstrap";
+import {Card, Collapse, ListGroup, ListGroupItem, Badge } from "reactstrap";
 
 export default class ActivityFeed extends React.Component<IActivityFeedProps, IActivityFeedState> {
 
@@ -10,6 +10,8 @@ export default class ActivityFeed extends React.Component<IActivityFeedProps, IA
         super(props);
 
         this.toggleCollapse = this.toggleCollapse.bind(this);
+        this.generateBikeBadge = this.generateBikeBadge.bind(this);
+        this.generateLimitedClass = this.generateLimitedClass.bind(this);
 
         this.state = {
             feed: [],
@@ -27,42 +29,63 @@ export default class ActivityFeed extends React.Component<IActivityFeedProps, IA
         }
     } 
 
+    private generateLimitedClass(limited: number): JSX.Element {
+        if (limited === 1) {
+            return <Badge color="warning">Limited</Badge>;
+        } else {
+            return <Badge color="secondary">Normal</Badge>;
+        }
+    }
+
+    private generateBikeBadge(bikeFlag: boolean): JSX.Element {
+        if (bikeFlag) {
+            return <span className="badge badge-primary">Yes</span>;
+        } else {
+            return <span className="badge badge-danger">No</span>;
+        }
+    }
+
     private toggleCollapse(event: any): void {
         let collapse = this.state.collapse;
         collapse[event.target.dataset.abbr] = !collapse[event.target.dataset.abbr];
         this.setState({collapse: collapse});
     }
 
-    generateEstimates(estimates: IEstimate[]): JSX.Element[] {
+    private generateEstimates(estimates: IEstimate[]): JSX.Element[] {
         return estimates.map((estimate: IEstimate, index) => {
             return (
                 <Card key={"estimate-card" + index}>
                     <div key={"estimate-card-body" + index} className="station-row-info">
-                        <div key={"estimate-bike" + index}>Bike: {estimate.bikeflag}</div>
-                        <div key={"estimate-delay" + index}>Minutes Delayed: {estimate.delay}</div>
-                        <div key={"estimate-direction" + index}>Direction: {estimate.direction}</div>
-                        <div key={"estimate-length" + index}>Length of Train: {estimate.length}</div>
-                        <div key={"estimate-minutes" + index}>Minutes before Arrival: {estimate.minutes}</div>
-                        <div key={"estimate-platform" + index}>Platform: {estimate.platform}</div>
+                        <table className="table">
+                            <tbody>
+                                <tr><td className="station-col-left">Bike</td><td>{this.generateBikeBadge(estimate.bikeflag)}</td></tr>
+                                <tr><td className="station-col-left">Minutes Delayed</td><td>{estimate.delay}</td></tr>
+                                <tr><td className="station-col-left">Direction</td><td>{estimate.direction}</td></tr>
+                                <tr><td className="station-col-left">Length of Train</td><td>{estimate.length}</td></tr>
+                                <tr><td className="station-col-left">Minutes Before Arrival</td><td>{estimate.minutes}</td></tr>
+                                <tr><td className="station-col-left">Platform Number</td><td>{estimate.platform}</td></tr>
+                            </tbody>
+                        </table>
                     </div>
                 </Card>
             );
         });
     }
 
-    generateActivityRows(rows: IEtd[]): JSX.Element[] {
+    private generateActivityRows(rows: IEtd[]): JSX.Element[] {
         return rows.map((etd: IEtd, index: number) => {
             return (
-                <ListGroupItem key={"est" + index}  
-                            id="toggler" 
-                            data-abbr={etd.abbreviation}
-                            style={{ marginBottom: '1rem' }}
-                            onClick={this.toggleCollapse}>
-                        {etd.abbreviation} - {etd.destination}
+                <div key={"section" + index}>
+                    <ListGroupItem key={"est" + index}  
+                                id="toggler" 
+                                data-abbr={etd.abbreviation}
+                                onClick={this.toggleCollapse}>
+                            {etd.abbreviation} - {etd.destination} {this.generateLimitedClass(etd.limited)}
+                    </ListGroupItem>
                     <Collapse isOpen={!!this.state.collapse[etd.abbreviation]}>
                         {this.generateEstimates(etd.estimates)}
                     </Collapse>
-                </ListGroupItem>
+                </div>
             );            
         });
     }
